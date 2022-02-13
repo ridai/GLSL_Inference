18cd034f03f15455e4a03589f781673e85f76b07
michihiro naito <>
2022-02-13 15:27:36 +0900

    fix comments

--- task1/index.js
+++ task1/index.js
@@ -1,102 +1,115 @@
 /* eslint no-console:0 consistent-return:0 */
 "use strict";
 
+// シェーダをGPUにUploadしてコンパイルまで行う
 function createShader(gl, type, source) {
   var shader = gl.createShader(type);
+  // sourceで受け取ったシェーダ文字列をUpload
   gl.shaderSource(shader, source);
+  // シェーダをCompile
   gl.compileShader(shader);
   var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
   if (success) {
     return shader;
   }
-
+  // 失敗したら後処理してエラー出力
   console.log(gl.getShaderInfoLog(shader));
   gl.deleteShader(shader);
 }
-
+// 頂点シェーダとフラグメントシェーダをプログラムという単位にまとめてlinkさせる
 function createProgram(gl, vertexShader, fragmentShader) {
   var program = gl.createProgram();
+  // プログラムに頂点シェーダを与える
   gl.attachShader(program, vertexShader);
+  // プログラムにフラグメントシェーダを与える
   gl.attachShader(program, fragmentShader);
+  // WebGLにプログラムをlinkさせる
   gl.linkProgram(program);
   var success = gl.getProgramParameter(program, gl.LINK_STATUS);
   if (success) {
     return program;
   }
-
+  // 失敗したら後処理してエラー出力
   console.log(gl.getProgramInfoLog(program));
   gl.deleteProgram(program);
 }
 
 function main() {
-  // Get A WebGL context
+  // ===== 初期化処理 =====
+  // canvasタグを指定して取得
   var canvas = document.querySelector("#c");
   var gl = canvas.getContext("webgl");
   if (!gl) {
     return;
   }
 
-  // Get the strings for our GLSL shaders
+  // 頂点シェーダの定義を文字列として取得
   var vertexShaderSource = document.querySelector("#vertex-shader-2d").text;
+  // フラグメントシェーダの定義を文字列として取得
   var fragmentShaderSource = document.querySelector("#fragment-shader-2d").text;
 
-  // create GLSL shaders, upload the GLSL source, compile the shaders
+  // createShaderメソッドをcallしてコンパイルまで行う
   var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
   var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
 
-  // Link the two shaders into a program
+  // シェーダをプログラムにまとめ、WebGLにリンクさせる
   var program = createProgram(gl, vertexShader, fragmentShader);
 
-  // look up where the vertex data needs to go.
+  // 頂点シェーダに与える引数(a_position)を特定し、WebGLに認識させる
   var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
 
-  // Create a buffer and put three 2d clip space points in it
+  // a_positionはattibution属性(=buffer)であるため、buffer領域を定義する
   var positionBuffer = gl.createBuffer();
 
-  // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+  // buffer領域をWebGL固有の「ARRAY_BUFFER」フィールドに紐づける
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
+  // 実際に、a_positionに与えるデータを定義する
   var positions = [
     0, 0,
     0, 0.5,
+    0.7, 0.5,
     0.7, 0,
   ];
+  // bufferDataにて、上記positionsを、ARRAY_BUFFERに与える
+  // 第一引数: ARRAY_BUFFERの指定
+  // 第二引数: 与えるデータを指定。この時、厳密な型が必要なため、キャストを行なっている
+  // 第三引数: データの更新頻度を指定している。STATIC_DRAWは更新頻度低
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
-  // code above this line is initialization code.
-  // code below this line is rendering code.
-
+  // ===== レンダリング処理 =====
   webglUtils.resizeCanvasToDisplaySize(gl.canvas);
 
-  // Tell WebGL how to convert from clip space to pixels
+  // WebGL上で、クリップ空間(-1~1で表現される座標)をキャンバス座標に変換するために必要な情報を渡す
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
-  // Clear the canvas
+  // キャンバスの色を無色に初期化する
   gl.clearColor(0, 0, 0, 0);
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  // Tell it to use our program (pair of shaders)
+  // 使うプログラムを指定
   gl.useProgram(program);
 
-  // Turn on the attribute
+  // attribute属性の引数(=a_position)の入力を有効化する
   gl.enableVertexAttribArray(positionAttributeLocation);
 
-  // Bind the position buffer.
+  // buffer領域をWebGL固有の「ARRAY_BUFFER」フィールドに紐づける
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-  // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-  var size = 2;          // 2 components per iteration
-  var type = gl.FLOAT;   // the data is 32bit floats
-  var normalize = false; // don't normalize the data
-  var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
-  var offset = 0;        // start at the beginning of the buffer
+  // positionsデータをどのようにARRAY_BUFFERに搭載するかを定義する。
+  // 頂点シェーダでは、vec4(4次元のベクトル)を入力とするので、想定するデータフォーマットになるように整形する
+  var size = 2;          // positionsから2要素ずつ切り取る
+  var type = gl.FLOAT;   // 32bit float型
+  var normalize = false; // 正規化行わない
+  var stride = 0;        // 次のデータの切り出しを行う際の移動幅
+  var offset = 0;        // 最初の切り出しを行う際に読み飛ばす幅
   gl.vertexAttribPointer(
       positionAttributeLocation, size, type, normalize, stride, offset);
 
   // draw
-  var primitiveType = gl.TRIANGLES;
+  var primitiveType = gl.TRIANGLE_FAN; // 頂点4つを使って、三角形を2つ描画する
   var offset = 0;
-  var count = 3;
+  var count = 4; // 頂点4つ使って描画することを表す
   gl.drawArrays(primitiveType, offset, count);
 }
 
